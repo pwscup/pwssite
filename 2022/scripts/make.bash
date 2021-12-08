867f5f389c90f984f05f976c5fd05eeb0f300964
gghatano <adad.0405@gmail.com>
2021-12-09 06:17:39 +0900

    add 2022 files

---
+++ 2022/scripts/make.bash
@@ -0,0 +1,28 @@
+dir=$(dirname $0)
+
+
+## markdownの更新/新規作成があれば, pandocする
+ls $dir/../markdown/*.md | while read line
+do
+  echo "line: "$line
+  basefilename=$(echo $line | xargs basename | sed "s/.md$//")
+  echo "basefilename: "$basefilename
+
+  if [ "$line" -nt "$dir/../html/${basefilename}.html" ]; then
+    echo "created or updated"
+    bash $dir/pandoc.bash $basefilename
+    tidy -i -utf8 $dir/../html/${basefilename}.html > $dir/../html/${basefilename}.tmp.html
+
+    ## mv $dir/../html/${basefilename}.tmp.html $dir/../html/${basefilename}.html
+    mv $dir/../html/${basefilename}.tmp.html $dir/../html/${basefilename}.html
+    chmod 770 $dir/../html/*.html
+    chmod 770 $dir/../Images
+
+    ## 公開する
+    cp $dir/../html/${basefilename}.html $dir/../${basefilename}.html
+  fi
+done
+
+## 図のコピー
+cp -r $dir/../markdown/Images $dir/../
+
