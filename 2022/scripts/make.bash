0e10c8521531aabea8e1ccbbe7235825afa27738
gghatano <adad.0405@gmail.com>
2022-06-07 15:29:34 +0900

    コメントを付与しました

--- 2022/scripts/make.bash
+++ 2022/scripts/make.bash
@@ -1,20 +1,26 @@
 dir=$(dirname $0)
 
 
-## markdownの更新/新規作成があれば, pandocする
+## markdownの更新/新規作成があれば, pandocで md -> html変換する、という処理
 ls $dir/../markdown/*.md | while read line
 do
   echo "line: "$line
+  ## 拡張子なしファイル名の取得
   basefilename=$(echo $line | xargs basename | sed "s/.md$//")
   echo "basefilename: "$basefilename
-
+  
+  ## 更新か新規作成があれば、処理する
   if [ "$line" -nt "$dir/../html/${basefilename}.html" ]; then
     echo "created or updated"
+    ## md -> html変換
     bash $dir/pandoc.bash $basefilename
+    ## htmlファイルのインデントを整形する
     tidy -i -utf8 $dir/../html/${basefilename}.html > $dir/../html/${basefilename}.tmp.html
-
-    ## mv $dir/../html/${basefilename}.tmp.html $dir/../html/${basefilename}.html
+    
+    ## ファイルをhtml配置用のフォルダに配置する
     mv $dir/../html/${basefilename}.tmp.html $dir/../html/${basefilename}.html
+    
+    ## 権限を制御する
     chmod 770 $dir/../html/*.html
     chmod 770 $dir/../Images
 
