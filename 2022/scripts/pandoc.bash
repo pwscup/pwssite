50756fa4fc932116d1ea6f3623251653c9f9a15e
Yuichi NAKAMURA <yuichi@aoni.waseda.jp>
2022-08-12 06:10:04 +0900

    Title header generator (the rest part)

--- 2022/scripts/pandoc.bash
+++ 2022/scripts/pandoc.bash
@@ -1,53 +1,80 @@
 
+# 定数
+## デフォルトの title 要素
+readonly DEFAULT_TITLE="PWS"
+
 ## このファイルが置かれたフォルダ
-dir=$(dirname $0)
+readonly DIR=$(dirname $0)
 
 ## ファイル名を引数にする
-basefilename=$1
+readonly BASE_FILENAME=$1
 
 ## 一時ファイル用に、プロセス番号を使う
-PID=$$
+readonly PID=$$
 
-## ***.md -> ***.htmlを生成する
+## 出力先htmlファイル
+readonly OUTPUT=${DIR}/../html/${BASE_FILENAME}.html
 
-pandoc -f markdown-auto_identifiers -t html ${dir}/../markdown/${basefilename}.md > ${dir}/${PID}.${basefilename}.html
+## Headers
+### 日本語版
+readonly HEADER1="${DIR}/../template/header.html"
+readonly HEADER2="${DIR}/../template/header_afterTitle.html"
+### 英語版
+readonly HEADER1_E="${DIR}/../template/header_e.html"
+readonly HEADER2_E="${DIR}/../template/header_afterTitle_e.html"
 
-## headとtailの間に、***.mdをhtmlに変換したものを追加する処理
+## コンテンツ
+### htmlファイル生成元のMarkdownファイル
+readonly CONTENTS_MD=${DIR}/../markdown/${BASE_FILENAME}.md
+### htmlファイルの生成先
+readonly CONTENTS_HTML=${DIR}/${PID}.${BASE_FILENAME}.html
 
+## Footer
+readonly FOOTER=${DIR}/../template/footer.html
+
+# 関数
+## .mdファイルの最初の "# ..." 行からtitle要素を作成
+function create_title() {
+  ### title文字列の抽出
+  local title=$( grep -e '^# ' ${CONTENTS_MD} | head -n1 | sed -e 's;^# \(.*\)$;\1;g' )
+  ### 抽出失敗時はデフォルト値を代入
+  test -z "${title}" && title="${DEFAULT_TITLE}"
+  ### titleタグで挟んで返す。
+  echo "    <title>${title}</title>"
+}
+
+
+
+########## MAIN ##########
+# ***.md -> ***.htmlを生成する
+pandoc -f markdown-auto_identifiers -t html ${CONTENTS_MD} > ${CONTENTS_HTML}
+
+# headとtailの間に、***.mdをhtmlに変換したものを追加する処理
 ## ヘッダの挿入
-### _eで終わるファイルのヘッダは英語版に
-if test ${basefilename: -2} = '_e'; then
-  if test ${basefilename:0:3} = 'cup'; then
-    ## cup_e用のヘッダ
-    cat $dir/../template/header_cup_e.html > $dir/../html/${basefilename}.html
-  else
-    ## PWS_e用のヘッダ
-    cat $dir/../template/header_e.html > $dir/../html/${basefilename}.html
-  fi
+if test ${BASE_FILENAME: -2} = '_e'; then
+  ### 英語版
+  cat ${HEADER1_E}  > ${OUTPUT}
+  $(create_title)  >> ${OUTPUT}
+  cat ${HEADER2_E} >> ${OUTPUT}
 else
-  if test ${basefilename:0:3} = 'cup'; then
-    ## cup用のヘッダ
-    cat $dir/../template/header_cup.html > $dir/../html/${basefilename}.html
-  else
-    ## PWS用のヘッダ
-    cat $dir/../template/header.html > $dir/../html/${basefilename}.html
-  fi
+  ### 日本語版
+  cat ${HEADER1}    > ${OUTPUT}
+  $(create_title)  >> ${OUTPUT}
+  cat ${HEADER2}   >> ${OUTPUT}
 fi
 
-## コンテンツの挿入
-echo "<!-- contents start -->" >> $dir/../html/${basefilename}.html
+# コンテンツの挿入
+echo "<!-- contents start -->" >> ${OUTPUT}
 
-cat $dir/${PID}.${basefilename}.html >> $dir/../html/${basefilename}.html
+cat ${CONTENTS_HTML}           >> ${OUTPUT}
 
-echo "" >> $dir/../html/${basefilename}.html
+echo ""                        >> ${OUTPUT}
 
-echo "<!-- contents end -->" >> $dir/../html/${basefilename}.html
+echo "<!-- contents end -->"   >> ${OUTPUT}
 
-## フッターの挿入
-cat $dir/../template/footer.html >> $dir/../html/${basefilename}.html
+# フッターの挿入
+cat ${FOOTER}                  >> ${OUTPUT}
 
-## tmpファイルの削除
-rm $dir/${PID}.*
-
-
+# tmpファイルの削除
+rm ${DIR}/${PID}.*
 
