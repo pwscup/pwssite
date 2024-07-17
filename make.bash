bea75557de3053fe9b06f2e8cd2bab22b4b774fe
gghatano <adad.0405@gmail.com>
2024-07-17 18:00:58 +0900

    Update make.bash

--- make.bash
+++ make.bash
@@ -2,7 +2,6 @@ dir=$(dirname $0)
 
 ## 更新がありそうなディレクトリを, 
 ## ${dir}/***/scripts/make.bash として指定する
-bash ${dir}/2023/scripts/make.bash
 bash ${dir}/2024/scripts/make.bash
 bash ${dir}/ipws2024/scripts/make.bash
 bash ${dir}/ppsd/scripts/make.bash
