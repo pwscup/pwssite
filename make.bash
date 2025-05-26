8225fc789aec0cf55139a41a77436d116a095366
gghatano <adad.0405@gmail.com>
2025-05-27 08:02:17 +0900

    ADD 2025 folder

--- make.bash
+++ make.bash
@@ -3,5 +3,6 @@ dir=$(dirname $0)
 ## 更新がありそうなディレクトリを, 
 ## ${dir}/***/scripts/make.bash として指定する
 bash ${dir}/2024/scripts/make.bash
+bash ${dir}/2025/scripts/make.bash
 bash ${dir}/ipws2024/scripts/make.bash
 bash ${dir}/ppsd/scripts/make.bash
