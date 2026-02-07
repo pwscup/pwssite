b33e8ad2fe2d71253570d81c145b9f546ad85462
gghatano <adad.0405@gmail.com>
2026-02-08 07:44:30 +0900

    Replace 2026 make.bash with make.py

--- make.bash
+++ make.bash
@@ -3,5 +3,5 @@ dir=$(dirname $0)
 ## 更新がありそうなディレクトリを, 
 ## ${dir}/***/scripts/make.bash として指定する
 bash ${dir}/2025/scripts/make.bash
-bash ${dir}/2026/scripts/make.bash
+python3 ${dir}/2026/scripts/make.py
 bash ${dir}/ppsd/scripts/make.bash
